7e7c53cc4fa7f4ae0af433955866e35078f06bc1
Alexander J Petri <57010524+aljpetri@users.noreply.github.com>
2022-10-03 17:55:10 +0300

    Added snakefile to generate PrecisionRecall for Rattle and Isonform

---
+++ Evaluation/SIRV_Precision_Recall/RunIsoncorrectIsonform.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+#Author: Alexander Petri
+# Modifications by Kristoffer Sahlin
+# This shell program is used to run longterm tests on the algorithm. 
+# For each amount of isoforms, this script performs 5 individual runs of 
+# generating test data and running the algorithm on it. To find the remaining 
+# bugs the script also outputs the test data files, which the algorithm did 
+# not work on correctly
+# RUN script as: ./generateTestResults.sh Max isoform number nr runs /path/to/input/reference.fa output_root /path/to/isONform.py
+#TODO invoke isONcorrect (Problem: we will need to generate folders for each read to fulfill the isONcorrect input scheme)
+if [ $# -lt 2 ]; then
+    # TODO: print usage
+    echo "./RunIsonform.sh  <Max isoform number> <nr runs> </path/to/input/reference.fa> <output_root> </path/to/isONform.py>"
+    exit 1
+fi
+max_iso_nr=$1
+nr_runs=$2
+input_ref=$3
+filedirectory=$4
+isonform_dir=$5
+mkdir -p $filedirectory
+#mkdir -p $filedirectory/errors
+mkdir -p $filedirectory/reads
+#mkdir -p $filedirectory/isONform
+outputfile=$filedirectory/CorrectForm.tsv
+echo $filedirectory
+#if results.tsv already exists 
+if [ -s $outputfile ]
+then 
+#delete all data from the file 
+   > $outputfile
+else
+#add a file results.tsv to write into
+   touch $outputfile
+fi
+#counter used to name error files correctly
+errorcounter=0
+#write nice header into the output file
+#echo -e "Number of Isoforms \t Found isoforms by IsONform \n">>results.tsv
+#ls
+#define the file we want to use as indicator for our algos performance
+file=out/mapping.txt
+file=$filedirectory/isONform/mapping.txt
+num_cores=8
+FILES=$filedirectory"/reads/*.fastq"
+echo $FILES
+start=$(date +%s)
+for file in $FILES
+do
+	echo $file
+	# cp $filedirectory/reads/reads.fq $filedirectory
+		#we want to figure out how many reads were actually generated
+		#read_amount=$(< $filedirectory/reads/reads.fq wc -l)
+		#
+		
+	part2=$(basename "$file")
+	echo "Part2"
+	echo "$part2"
+	arrIN=(${part2//./ })
+	echo ${arrIN[0]}  
+	fname=(${part2//./ })
+	echo ${fname[0]}
+	fnamesplit=(${fname[0]//_/ })
+	i=${fnamesplit[1]}
+	j=${fnamesplit[2]}
+	number="${i}_${j}"
+	echo "$i \t $j" 
+	read_amount=$(< $file wc -l)
+	#As fastq entries have 4 lines divide by 4 to get the actual number of reads
+	var=4
+	true_read_amount=$((read_amount / var))
+		#mv $filedirectory/reads.fq $filedirectory/reads_$number.fq
+		#run IsONform
+		#if e=True
+	echo $isonform_dir/main.py
+	isONcorrect  --fastq $file  --outfolder $filedirectory/correction/
+	mv $filedirectory/correction/corrected_reads.fastq $filedirectory/correction/corr_$number.fastq
+	python $isonform_dir/main.py --fastq $filedirectory/correction/corr_$number.fastq --k 9 --w 20 --xmin 14 --xmax 80 --exact --max_seqs_to_spoa 200 --delta_len 5 --outfolder $filedirectory/isONcorrform
+	#python -m pyinstrument main.py --fastq $filedirectory/reads_$number.fq --k 9 --w 10 --xmin 14 --xmax 80 --exact --max_seqs_to_spoa 200 --delta_len 5 --outfolder $filedirectory/isonform/
+		#if e=False
+		#python main.py --fastq $filedirectory/reads/isoforms.fa --k 9 --w 10 --xmin 14 --xmax 80 --exact --max_seqs_to_spoa 200 --delta_len 3 --outfolder out
+		had_issue=$?
+		zero=0
+		echo $had_issue
+		#python main.py --fastq $filedirectory/reads/isoforms.fa --k 9 --w 10 --xmin 14 --xmax 80 --exact --max_seqs_to_spoa 200 --max_bubblesize 2 --delta_len 3 --outfolder testout 
+		#we count the lines in mapping.txt, which is an output of IsONform. As we have a fasta file we have to divide this number by 2 to retreive the actual number of isoforms we generated
+		#TODO: we have to change $file to another variable holding the name "transcriptome_mapping.txt"
+		res=$(< "$filedirectory/isONcorrform/cluster_mapping.txt" wc -l)
+		cp $filedirectory/isONcorrform/cluster_mapping.txt $filedirectory/isONcorrform/cl_map_$number.txt
+		cp $filedirectory/isONcorrform/cluster_merged.fastq $filedirectory/isONcorrform/cl_spoa_$number.fastq
+		result=$(($res/2))
+		if [[ "$had_issue" != "$zero" ]]
+		then
+		errorcounter=$((errorcounter+1))
+		result=-1
+		cp $file $filedirectory/errors/error_${errorcounter}.fq
+		#cp $filedirectory/reads/reads.fq $filedirectory/errors/error_${errorcounter}_$number.fq
+
+		fi
+		#echo "$result"
+		echo -e "$i \t $j \t $result \t $true_read_amount \t" >> $outputfile
+		num2=2
+		otherres=$((result * num2))
+done
+end=$(date +%s)
+echo "Elapsed Time: $(($end-$start)) seconds"	
---
+++ Evaluation/SIRV_Precision_Recall/sirv_subsample_isoforms.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+
+import argparse
+import sys, os
+import random
+import pysam
+import errno
+from collections import defaultdict
+
+import argparse
+import errno
+"""
+sirv_subsample_isoform: A script to subsample reads from a SIRV dataset according to a minimap alignment
+USAGE:  python sirv_subsample_isoform.py --fastq {inputfile} --alignments {alignmentfile} --outfile {outfile}
+
+"""
+'''
+    Below code taken from https://github.com/lh3/readfq/blob/master/readfq.py
+'''
+
+def readfq(fp): # this is a generator function
+    last = None # this is a buffer keeping the last unprocessed line
+    while True: # mimic closure; is it a bad idea?
+        if not last: # the first record or a record following a fastq
+            for l in fp: # search for the start of the next record
+                if l[0] in '>@': # fasta/q header line
+                    last = l[:-1] # save this line
+                    break
+        if not last: break
+        name, seqs, last = last[1:].split()[0], [], None
+        for l in fp: # read the sequence
+            if l[0] in '@+>':
+                last = l[:-1]
+                break
+            seqs.append(l[:-1])
+        if not last or last[0] != '+': # this is a fasta record
+            yield name, (''.join(seqs), None) # yield a fasta record
+            if not last: break
+        else: # this is a fastq record
+            seq, leng, seqs = ''.join(seqs), 0, []
+            for l in fp: # read the quality
+                seqs.append(l[:-1])
+                leng += len(l) - 1
+                if leng >= len(seq): # have read enough quality
+                    last = None
+                    yield name, (seq, ''.join(seqs)); # yield a fastq record
+                    break
+            if last: # reach EOF before reading enough quality
+                yield name, (seq, None) # yield a fasta record instead
+                break
+
+
+def get_subsamples(transcript_cov, depth_dist, nr_isoforms):
+    #print("TC",transcript_cov)
+    subsamples = {}
+    already_used=set()
+    depth_list=[]
+    for i in range(0,nr_isoforms):
+        if depth_dist=="uniform":
+            depth=random.randrange(10,50)
+        elif depth_dist=="exp":
+            depth=random.choice([1,2,4,8,16,32,64,128])
+        depth_list.append(depth)
+    for depth_elem in depth_list:
+    #for gene_id in transcript_cov:
+        not_set = True
+        while not_set:
+            tr_acc = random.sample(list(transcript_cov), 1)
+            this_acc=tr_acc[0]
+            if len(transcript_cov[this_acc]) > depth_elem:
+                #print("LEN",len(transcript_cov[this_acc]))
+                if not this_acc in already_used:
+                    subset = random.sample(transcript_cov[this_acc], depth_elem) # this is without replacement
+                    subsamples[this_acc] = subset
+                    not_set=False
+                    already_used.add(this_acc)
+                    print("Sampled ", depth_elem," reads for ",this_acc)
+    return subsamples
+
+    # subsamples = {}
+    # already_ampled = set()
+    # for tr_acc in transcript_cov:
+    #     print(tr_acc, len(transcript_cov[tr_acc]))
+    #     subset = random.sample(transcript_cov[tr_acc], depth) # this is without replacement
+    #     subsamples[tr_acc] = subset
+    # return subsamples
+
+
+
+def get_abundance_aligned_reads(sam_file):
+    SAM_file = pysam.AlignmentFile(sam_file, "r", check_sq=False)
+    references = SAM_file.references
+    valid_genes = set(["SIRV1","SIRV2","SIRV3","SIRV4", "SIRV5", "SIRV6","SIRV7"])
+    transcript_cov = {}
+    # amgiguous_primary = defaultdict(set)
+
+    for read in SAM_file.fetch(until_eof=True):
+        if (read.flag == 0 or read.flag == 16):
+            transcript_id = read.reference_name
+            if transcript_id[:5] in valid_genes:
+                #gene_id = transcript_id[4]
+                #transcript_cov[gene_id][transcript_id].add(read.query_name)
+                if not transcript_id in transcript_cov:
+                    e_list=[]
+                    e_list.append(read.query_name)
+                    transcript_cov[transcript_id]=e_list
+                else:
+                    lista=transcript_cov[transcript_id]
+                    lista.append(read.query_name)
+                    up_dict={transcript_id: lista}
+                    transcript_cov.update(up_dict)
+                #transcript_cov[transcript_id].add(read.query_name)
+    # except:
+    #     pass
+
+    # print(transcript_cov)
+
+        # elif (read.flag == 0 or read.flag == 16) and read.mapping_quality == 0:
+        #     transcript_id = read.reference_name
+        #     amgiguous_primary[transcript_id].add(read.query_name)
+
+    return transcript_cov
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+        print("creating", path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def main(args):
+    dirname = os.path.dirname(args.outfile)
+    mkdir_p(dirname)
+    transcript_cov = get_abundance_aligned_reads(args.alignments)
+    #print(len(transcript_cov), [len(transcript_cov[g]) for g in transcript_cov])
+    subsamples = get_subsamples(transcript_cov, args.depth_dist, args.nr_isoforms)
+    fastq = { acc : (seq,qual) for acc, (seq,qual) in readfq(open(args.fastq, 'r'))}
+
+    outfile = open(args.outfile, "w")
+    for tr_id, set_of_reads in subsamples.items():
+        for read_acc in set_of_reads:
+            seq, qual  = fastq[read_acc]
+            outfile.write("@{0}\n{1}\n+\n{2}\n".format(read_acc, seq, qual))
+
+    outfile.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Parses alignments and subsamples a fastq file based on alignments.")
+    parser.add_argument('--fastq', type=str, help='fastq file. ')
+    # parser.add_argument('ref_fastq', type=str, help='fastq file. ')
+    parser.add_argument('--alignments', type=str, help='fastq file. ')
+    parser.add_argument('--outfile', type=str, help='Fastq file. ')
+    parser.add_argument('--depth_dist', type=str, help="type of read distribution: either uniform (20 reads per isoform) or exp (2^(x-2)|x<=10)")
+    parser.add_argument('--nr_isoforms', type=int, help='Number of nr_isoforms.')
+    args = parser.parse_args()
+    main(args)
