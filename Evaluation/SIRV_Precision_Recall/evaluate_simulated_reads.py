bfae6ba04e75c11e4d1f92dd3af726907d2da021
Alexander J Petri <57010524+aljpetri@users.noreply.github.com>
2022-10-15 17:25:29 +0300

    Finalized the pipeline.

--- Evaluation/SIRV_Precision_Recall/evaluate_simulated_reads.py
+++ Evaluation/SIRV_Precision_Recall/evaluate_simulated_reads.py
@@ -5,7 +5,7 @@ import re
 # import ssw
 import numpy as np
 from Bio import pairwise2
-from Bio.SubsMat import MatrixInfo as matlist
+#from Bio.SubsMat import MatrixInfo as matlist
 import pandas as pd
 import string
 import fractions
@@ -95,19 +95,83 @@ def edlib_ed(x, y, mode="NW", task="distance", k=1):
     #print("result",result)
     ed = result["editDistance"]
     return ed
+def reference_abundances(sampled_transcripts, outfolder, params):
+    seqs_seen = set()
+    transcript_abundances = defaultdict(int)
+    for acc, seq in sorted(sampled_transcripts.items(), key=lambda x: len(x[1])):
+        acc_mod = acc.split("_")[0]
+        transcript_abundances[acc_mod] += 1
 
+    # sorted_reference_tuples = [ (acc.split("_")[0], transcript_abundances[acc.split("_")[0]]) for acc, seq in sorted(sampled_transcripts.items(), key = lambda x: len(x[1]))]
+    sorted_reference_tuples = [ (acc, ab) for acc, ab in transcript_abundances.items()]
+    reference_abundances = [0]*len(sorted_reference_tuples)
+    for i, (acc1,ab1) in enumerate(sorted_reference_tuples):
+        reference_abundances[i] = [0]*len(sorted_reference_tuples)
+        for j, (acc2,ab2) in enumerate(sorted_reference_tuples):
+            reference_abundances[i][j] = float(ab1)/ab2
+
+    relative_abundance_matrix_data_frame = pd.DataFrame(reference_abundances)
+    # msk = relative_abundance_matrix_data_frame > 99
+    # relative_abundance_matrix_data_frame_masked = relative_abundance_matrix_data_frame.mask(msk)
+    plot_heatmap("relative_abundance", relative_abundance_matrix_data_frame, outfolder)
+    # plot_heatmap("relative_abundance", relative_abundance_matrix_data_frame, annotation=True)
+
+def reference_similarity(reference_transcripts, outfolder, params):
+    """
+        Stats about reference transcripts
+    """
+
+    reference_similarities = []
+    if not params.no_ref_sim:
+
+        print("calculating reference similarities")
+        # do SW
+        sorted_reference_tuples = sorted(reference_transcripts.items(), key = lambda x: len(x[1]))
+        reference_similarities = [0]*len(sorted_reference_tuples)
+        for i, (q_acc, q_seq) in enumerate(sorted_reference_tuples):
+            reference_similarities[i] = [0]*len(sorted_reference_tuples)
+            # print("ref", i)
+            for j, (r_acc, r_seq) in enumerate(sorted_reference_tuples):
+                # if len(q_seq) - len(r_seq) > 99 or len(q_seq) - len(r_seq) < 99:
+                #     reference_similarities[q_acc][r_acc] = min(len(q_seq), len(r_seq))
+                #     continue
+
+                ed = edlib_ed(q_seq, r_seq, mode="NW", task="distance", k=2*max(len(q_seq), len(r_seq) ) )
+                reference_similarities[i][j] = ed
+
+                # r_aligned, q_aligned, stats = ssw_alignment( q_acc, r_acc, q_seq, r_seq, i,j, max_discrepancy = 10000 )
+                # if stats:
+                #     matches, mismatches, indels, deletions, insertions = stats
+                #     errors = mismatches + indels
+                #     identity = errors/ float(errors + matches)
+                #     reference_similarities[i][j] = errors
+                #     # print(ed, errors)
+                # else:
+                #     reference_similarities[i][j] = min(len(q_seq), len(r_seq))
+
+
+
+        ref_sim_data_frame = pd.DataFrame(reference_similarities)
+        msk = ref_sim_data_frame > 99
+        ref_sim_data_frame_masked = ref_sim_data_frame.mask(msk)
+        plot_heatmap("similarities", ref_sim_data_frame_masked, outfolder)
+
+    return reference_similarities
 
 def get_minimizers_2set_simple(querys, targets):
     best_edit_distances = {}
 
     for acc1, seq1 in querys.items():
-        #print(acc1)
         best_ed = 100000 #len(seq1)
         for acc2, seq2 in targets.items():
-                #print(acc2)
+                print(acc2)
+                print(seq1)
+                print(seq2)
             #if acc1[:3] == acc2[:3]: #same gene family
-                edit_distance = edlib_ed(seq1, seq2, mode="NW", k = 2*len(seq1)) # seq1 = query, seq2 = target
-                #print(edit_distance)
+                edit_distance = edlib_ed(seq1, seq2, mode="NW", k=best_ed)#k = 2*len(seq1)) # seq1 = query, seq2 = target
+
+                print("best_Ed",best_ed)
+                print(edit_distance)
                 if 0 <= edit_distance < best_ed:
                     best_ed = edit_distance
                     #print("BESTED",best_ed)
@@ -115,6 +179,9 @@ def get_minimizers_2set_simple(querys, targets):
                     best_edit_distances[acc1][acc2] = best_ed
                 elif edit_distance == best_ed:
                     best_edit_distances[acc1][acc2] = best_ed
+                else:
+                    print(acc1,acc2)
+                    print("again wrong")
     return best_edit_distances
 
 
@@ -179,9 +246,10 @@ def parasail_alignment(read, reference, x_acc = "", y_acc = "", match_score = 2,
 
 def get_alignments(best_edit_distances, querys, targets):
     best_edit_distances_parasail = {}
+    #print("BED", best_edit_distances)
     for acc1 in best_edit_distances:
         seq1 = querys[acc1]
-        best_ed = len(seq1)
+        best_ed = 100000000000#len(seq1)
         best_edit_distances_parasail[acc1] = {}
 
         for acc2 in best_edit_distances[acc1]:
@@ -196,6 +264,7 @@ def get_alignments(best_edit_distances, querys, targets):
             deletions_minus_ends = read_alignment[20:-20].count("-")
 
             sw_ed = mismatches + indels
+            #print(sw_ed, best_ed)
             if sw_ed < best_ed:
                 best_edit_distances_parasail[acc1] = {}
                 best_edit_distances_parasail[acc1][acc2] = (deletions, insertions, mismatches)
@@ -203,15 +272,16 @@ def get_alignments(best_edit_distances, querys, targets):
 
             elif sw_ed == best_ed:
                 best_edit_distances_parasail[acc1][acc2] = (deletions, insertions, mismatches)
-
+            else:
+                print("HERE",acc1,acc2)
             # seq1_aln, match_line, seq2_aln = result.alignment
-
+    #print(best_edit_distances_parasail)
     return best_edit_distances_parasail
 
 
 def get_best_match(corrected_reads, reference_transcripts, params,outfile):
     print("getting_best match")
-    print("OUTFILE",outfile)
+    #print("OUTFILE",outfile)
     out_file = open(outfile, "w")
     errors_container = {}
     error_rate_container = {}
@@ -268,7 +338,7 @@ def get_best_match(corrected_reads, reference_transcripts, params,outfile):
                 print("TIE!!", q_acc, minimizer_graph_c_to_t[q_acc])
                 if q_acc_mod in minimizer_graph_c_to_t[q_acc]:
                     tmp = minimizer_graph_c_to_t[q_acc][q_acc_mod]
-                    #print(tmp)
+                    #print("TMP",tmp)
                     minimizer_graph_c_to_t[q_acc] = {}
                     minimizer_graph_c_to_t[q_acc] = { q_acc_mod : tmp}
                     print("Done")
@@ -329,6 +399,7 @@ def get_best_match(corrected_reads, reference_transcripts, params,outfile):
     all_d = 0
     total_read_nucleotides = 0
     for q_acc, q_seq in corrected_reads.items():
+        #print(q_acc,q_seq)
         q_acc_mod = q_acc.split("_")[0]
         # if q_acc_mod == best_match_container[q_acc]:
         tot_errors += errors_container[q_acc]
@@ -368,41 +439,42 @@ def get_best_match(corrected_reads, reference_transcripts, params,outfile):
 #TODO:figure out whether soft-clips were included
 #get total errors for each of the best fitting transcripts. Get average error for all transcripts aligned to original transcripts. Rewiever version: split up error types
 def main(args):
+    #print(args.corrected_reads)
     corrected_reads = {acc: seq.upper() for acc, (seq, _) in readfq(open(args.corrected_reads, 'r'))}
     reference_transcripts = {acc: seq.upper() for acc, (seq, _) in readfq(open(args.reference_transcripts, 'r'))}
-    
-    get_best_match(corrected_reads, reference_transcripts, args,args.outfile)
 
-    #out_file_ref_sim = open(os.path.join(args.outfolder, "ref_similaritiy_distr.csv"), "w")
+    get_best_match(corrected_reads, reference_transcripts, args, args.outfile)
+
+    # out_file_ref_sim = open(os.path.join(args.outfolder, "ref_similaritiy_distr.csv"), "w")
 
     """if not args.no_ref_sim:
         ref_similarities = []
-        for i in range(len(reference_similarities)):
-            temp_list = reference_similarities[i]
-            del temp_list[i]
-            min_sim_to_i = min(temp_list) 
-            ref_similarities.append(min_sim_to_i)
+        #for i in range(len(reference_similarities)):
+        #    temp_list = reference_similarities[i]
+        #    del temp_list[i]
+        #    min_sim_to_i = min(temp_list)
+        #    ref_similarities.append(min_sim_to_i)
             # for j in range(len(reference_similarities[i])):
             #     if i <= j:
             #         continue
             #     else:
             #         ref_similarities.append(reference_similarities[i][j])
 
-        n = float( len(ref_similarities) )
-        mu =  sum(ref_similarities) / n
+        n = float(len(ref_similarities))
+        mu = sum(ref_similarities) / n
         sigma = (sum(list(map((lambda x: x ** 2 - 2 * x * mu + mu ** 2), ref_similarities))) / (n - 1)) ** 0.5
         min_error = min(ref_similarities)
         max_error = max(ref_similarities)
         ref_similarities.sort()
-        if len(ref_similarities) %2 == 0:
-            median_error = (ref_similarities[int(len(ref_similarities)/2)-1] + ref_similarities[int(len(ref_similarities)/2)]) / 2.0
+        if len(ref_similarities) % 2 == 0:
+            median_error = (ref_similarities[int(len(ref_similarities) / 2) - 1] + ref_similarities[
+                int(len(ref_similarities) / 2)]) / 2.0
         else:
-            median_error = ref_similarities[int(len(ref_similarities)/2)]"""
+            median_error = ref_similarities[int(len(ref_similarities) / 2)]
 
-        #out_file_ref_sim.write("mean_error\tsd_error\tmin_error\tmax_error\tmedian_error\n")
-        #out_file_ref_sim.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(mu, sigma, min_error, max_error, median_error))
-
-
+        out_file_ref_sim.write("mean_error\tsd_error\tmin_error\tmax_error\tmedian_error\n")
+        out_file_ref_sim.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(mu, sigma, min_error, max_error, median_error))
+"""
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Evaluate pacbio IsoSeq transcripts.")
@@ -422,5 +494,5 @@ if __name__ == '__main__':
 
     #outfolder = args.outfolder
     #if not os.path.exists(outfolder):
-    #    os.makedirs(outfolder)
-    main(args)
+    #    os.makedirs(outfolder)"""
+main(args)
