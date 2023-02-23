e21a977d4872c8dd46505eecbc6089debea16044
aljpetri <aljpetri@gmail.com>
2023-02-23 12:05:49 +0200

    Added the analysis pipeline suitable for human PacBio data

---
+++ Evaluation/Human/Pacbio/pipeline_no_pychop.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+set -e
+if [ $# -ne 5 ]; then
+        echo "Usage: `basename $0`  <raw_reads.fq>  <outfolder>  <num_cores> "
+        exit 0
+fi
+
+raw_reads=$1
+outfolder=$2
+num_cores=$3
+isONform_folder=$4
+iso_abundance=$5
+echo "Running: " `basename $0` $raw_reads $outfolder $num_cores $isONform_folder $iso_abundance
+#isonform_folder=${isONform_folder::-1}
+mkdir -p $outfolder
+echo "ISONfolder "$isONform_folder
+echo "Outfolder "$outfolder
+echo "Will run pychopper (cdna_classifier.py), isONclust, isONcorrect and isONform. Make sure you have these tools installed."
+echo "For installation see: https://github.com/ksahlin/isONcorrect#installation and  https://github.com/aljpetri/isONform"
+echo
+
+#echo
+#echo "Running pychopper"
+#echo
+
+#pychopper  $raw_reads $outfolder/full_length.fq -t $num_cores
+
+#echo
+#echo "Finished pychopper"
+#echo
+
+
+
+echo
+echo "Running isONclust"
+echo
+
+/usr/bin/time -v isONclust  --t $num_cores  --ont --fastq $raw_reads \
+             --outfolder $outfolder/clustering --k 8 --w 9 
+#ONT: k 14 w 20
+/usr/bin/time -v isONclust write_fastq --clusters $outfolder/clustering/final_clusters.tsv \
+                     --fastq $raw_reads --outfolder  $outfolder/clustering/fastq_files --N $iso_abundance
+echo
+echo "Finished isONclust"
+echo
+
+
+#echo
+#echo "Filtering clusters"
+#echo
+#python3.11 $isONform_folder/filter_clusters_by_size.py --in_folder $outfolder/clustering/fastq_files --abundance $iso_abundance
+
+#echo
+#echo "Filtering done"
+#echo
+echo
+echo "Running isONcorrect"
+echo
+#run_isoncorrect --t $num_cores  --fastq_folder $raw_reads  --outfolder $outfolder/correction/
+
+
+#the following line is without isONclust: This has to be included again
+/usr/bin/time -v python3.11 run_isoncorrect --t $num_cores  --fastq_folder $outfolder/clustering/fastq_files  --outfolder $outfolder/correction/
+
+echo
+echo "Finished isONcorrect"
+echo
+
+
+echo
+echo "Running isONform"
+echo
+#python3.11 $isONform_folder/isONform_parallel.py --fastq_folder $outfolder/correction/ --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 5 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance --split_wrt_batches
+#python3.11 $isONform_folder/main.py --fastq $outfolder/correction/*/*.fastq --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 5 --outfolder $outfolder/singleisoforms --iso_abundance $iso_abundance
+/usr/bin/time -v python3.11 $isONform_folder/isONform_parallel.py --t $num_cores --fastq_folder $outfolder/correction/ --exact_instance_limit 50 --k 20 --w 31 --xmin 14 --xmax 80 --max_seqs_to_spoa 200 --delta_len 10 --outfolder $outfolder/isoforms --iso_abundance $iso_abundance --split_wrt_batches --merge_sub_isoforms_3  --merge_sub_isoforms_5 --delta_iso_len_3 30 --delta_iso_len_5 50 --slow
+echo
+echo "Finished isONform"
+echo
+# OPTIONAL BELOW TO MERGE ALL CORRECTED READS INTO ONE FILE
+#touch $outfolder/all_corrected_reads.fq
+#OUTFILES=$outfolder"/correction/"*"/corrected_reads.fastq"
+#for f in $OUTFILES
+#do
+#  echo $f
+#  cat $f >> $outfolder/all_corrected_reads.fq
+#done
+
+echo
+echo "Finished with pipeline and wrote corrected reads to: " $outfolder/isoforms
+echo
---
+++ Evaluation/Human/Pacbio/submit_main_snakemake.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#SBATCH -A snic2021-5-572
+#SBATCH --time=10-00:00:00
+#SBATCH -p core
+#SBATCH -n 1
+# Memory per node specification is in MB. It is optional. 
+# The default limit is 3000MB per core.
+#SBATCH --job-name="IsONformDrosophila"
+#SBATCH --mail-user=alexander.petri@math.su.se
+#SBATCH --mail-type=ALL
+
+#conda init bash
+set -o errexit
+
+# --forcerun minimap2_align minimap2_map bwa_mem_align strobemap_align strobemap_map accelalign_align accelalign_map bowtie2_align
+module load gcc/9.3.0
+
+snakemake --keep-going -j 999999 --cluster "sbatch -A {cluster.account} -C {cluster.C} -c {cluster.cpus-per-task} -N {cluster.Nodes}  -t {cluster.runtime} -J {cluster.jobname} --mail-type={cluster.mail_type} --mail-user={cluster.mail}" --cluster-config cluster.json --configfile cluster_config.json --latency-wait 100 --verbose -n
